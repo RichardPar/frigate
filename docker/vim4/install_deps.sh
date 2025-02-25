34c61494f72f6cdd3cce5101179e31d103efad0a
RichardPar <tuxbox.guru@gmail.com>
2025-02-25 16:22:34 +0000

    Fixes for updates

--- docker/vim4/install_deps.sh
+++ docker/vim4/install_deps.sh
@@ -14,18 +14,21 @@ apt-get -qq install --no-install-recommends -y \
     curl \
     jq \
     nethogs \
-    libwayland-server0
+    libwayland-server0 \
+    libgomp1
 
 mkdir -p -m 600 /root/.gnupg
 
 # armNN_GPU -> arm64
 # Khadas VIM4 specific at this point!
 if [[ "${TARGETARCH}" == "arm64" ]]; then
-   wget -c "https://github.com/ARM-software/armnn/releases/download/v23.02/ArmNN-linux-aarch64.tar.gz"
-   mkdir /lib/armnn
-   tar -xf ArmNN*.tar.gz -C /lib/armnn
+   #wget -c "https://github.com/ARM-software/armnn/releases/download/v23.02/ArmNN-linux-aarch64.tar.gz"
+   wget -c "https://github.com/ARM-software/armnn/releases/download/v24.11/ArmNN-linux-aarch64.tar.gz"
+   mkdir /usr/lib/armnn
+   tar -xf ArmNN*.tar.gz -C /usr/lib/armnn
    mkdir -p /etc/ld.so.conf.d | true 
-   echo "/usr/lib/armnn" > /etc/ld.so.conf.d/armnn.conf
+   echo "/usr/lib/armnn/delegate" > /etc/ld.so.conf.d/armnn.conf
+   echo "/usr/lib/armnn" >> /etc/ld.so.conf.d/armnn.conf
    wget "https://dl.khadas.com/repos/vim4/pool/main/l/linux-gpu-mali-wayland/linux-gpu-mali-wayland_1.1-r37p0-202208_arm64.deb"
    dpkg -i linux-gpu-mali-wayland_1.1-r37p0-202208_arm64.deb
 fi
